a65707bce63f9d2f5e74b251bd128cbf0bebee0d
sydsainy <syedhusaini141208@gmail.com>
2026-07-02 20:29:29 +0800

    Update about-me.cpp

--- about-me.cpp
+++ about-me.cpp
@@ -6,9 +6,9 @@ int main() {
     cout << "          About Myself              " << endl;
     cout << "====================================" << endl;
 
-    cout << "Hi, my name is Sayyid." << endl;
+    cout << "Hi, my name is Syed." << endl;
     cout << "I am a student in Diploma Teknologi Maklumat." << endl;
-    cout << "I am currently learning programming fundamentals." << endl;
+    cout << "I am currently learning calculus and algebra." << endl;
     cout << "The main programming language I am learning is C++." << endl;
 
     cout << endl;
@@ -17,25 +17,6 @@ int main() {
     cout << "This is my first GitHub repository." << endl;
     cout << "I want to use GitHub to save my coding practice." << endl;
     cout << "I also want to track my progress from beginner to better." << endl;
-
-    cout << endl;
-
-    cout << "My learning goals are:" << endl;
-    cout << "1. Understand C++ step by step." << endl;
-    cout << "2. Improve my typing and coding skills." << endl;
-    cout << "3. Learn how to solve programming problems." << endl;
-    cout << "4. Become more confident in IT subjects." << endl;
-    cout << "5. Keep practicing until I improve." << endl;
-
-    cout << endl;
-
-    cout << "My message to myself:" << endl;
-    cout << "Do not rush. Learn slowly and clearly." << endl;
-    cout << "Every small practice is still progress." << endl;
-    cout << "Mistakes are part of learning programming." << endl;
-
-    cout << endl;
-
     cout << "Thank you for reading my first C++ program!" << endl;
 
     return 0;
